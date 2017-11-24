8adfb4d39e18393ab7ba838620c5209f9ba1c936
Oleg Dudar <oleg_dudar@epam.com>
2017-11-24 19:29:22 +0200

    Added feture for filter LoggedWork only for Specified Month. Added feature for input file name from cmd. Fixed some logic bugs.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,102 +5,139 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 import com.csvreader.CsvReader;
 
 public class Main {
 
-    public static void main (String[] args){
+    public static void main (String[] args) {
 
-        System.out.println("Application initialized.");
+        String pathToFile;
+        String pathToFileCreate;
+        String reportMonth;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Application started.");
+        while (true) {
+            String input;
 
-        String pathToFile = args[0];
-        String pathToFileCreate = args[1];
+            System.out.print("Enter input file name : ");
+            pathToFile = scanner.nextLine();
 
-        int indexForIssueKey = 0;
-        int indexForSummaryKey = 0;
-        int index = 0;
-        CsvReader products;
-        String[] headers;
-        List indexesForLoggedWork = new ArrayList();
-        List<TicketLoggedWork> records = new ArrayList<TicketLoggedWork>();
+            System.out.print("Enter out file name : ");
+            pathToFileCreate = scanner.nextLine();
 
-        try {
-            products = new CsvReader(pathToFile);
-            products.readHeaders();
-            headers = products.getHeaders();
-        }
-        catch (FileNotFoundException e) {
-            System.out.println("Could not found input file: " + args[0]);
-            e.printStackTrace();
-            return;
-        }
-        catch (IOException e1) {
-            System.out.println("Could not read headers from file: " + args[0]);
-            e1.printStackTrace();
-            return;
-        }
+            System.out.print("Enter month to calculate : ");
+            reportMonth = scanner.nextLine();
 
-        System.out.println("Transformation started ...");
-        for(String val : headers){
-            if(val.equals("Issue key")){
-                indexForIssueKey = index;
+            System.out.print("Enter [c] for calculate, or [q] for EXIT.");
+            input = scanner.nextLine();
+
+            if ("q".equals(input)) {
+                System.out.println("Exit!");
+                System.exit(0);
             }
-            if(val.equals("Log Work")){
-                indexesForLoggedWork.add(index);
+
+            if ("c".equals(input)) {
+                System.out.println("Going to Calculation.");
+                break;
             }
-            if(val.equals("Summary")){
-                indexForSummaryKey = index;
-            }
-            index++;
+
         }
 
-        try {
-            while (products.readRecord()) {
-                List<String> workLogged = new ArrayList<String>();
-                for (int i = 0; i < indexesForLoggedWork.size(); i++) {
-                    if (!products.get((Integer) indexesForLoggedWork.get(i)).equalsIgnoreCase(""))
-                        workLogged.add(products.get((Integer) indexesForLoggedWork.get(i)));
+        scanner.close();
+
+        if(pathToFileCreate.isEmpty()){
+            pathToFileCreate = "OUTPUT_RESULTS.csv";
+        }
+
+        if (!pathToFile.isEmpty()) {
+
+            int indexForIssueKey = 0;
+            int indexForSummaryKey = 0;
+            int index = 0;
+            CsvReader products;
+            String[] headers;
+            List indexesForLoggedWork = new ArrayList();
+            List<TicketLoggedWork> records = new ArrayList<TicketLoggedWork>();
+
+            try {
+                products = new CsvReader(pathToFile);
+                products.readHeaders();
+                headers = products.getHeaders();
+            } catch (FileNotFoundException e) {
+                System.out.println("Could not found input file: " + args[0]);
+                e.printStackTrace();
+                return;
+            } catch (IOException e1) {
+                System.out.println("Could not read headers from file: " + args[0]);
+                e1.printStackTrace();
+                return;
+            }
+
+            System.out.println("Transformation started ...");
+            for (String val : headers) {
+                if (val.equals("Issue key")) {
+                    indexForIssueKey = index;
                 }
-                records.add(new TicketLoggedWork(products.get(indexForIssueKey), workLogged, products.get(indexForSummaryKey)));
+                if (val.equals("Log Work")) {
+                    indexesForLoggedWork.add(index);
+                }
+                if (val.equals("Summary")) {
+                    indexForSummaryKey = index;
+                }
+                index++;
             }
-            products.close();
-        }
-        catch (IOException e2) {
-            System.out.println("Error occured while working with file: " + args[0]);
-            e2.printStackTrace();
-            return;
-        }
 
-        for(TicketLoggedWork item : records){
-            item.calculate();
-        }
-
-        PrintWriter pw = null;
-        try {
-            pw = new PrintWriter(new File(pathToFileCreate));
-        } catch (FileNotFoundException e3) {
-            System.out.println("Could not create file: " + args[1]);
-            e3.printStackTrace();
-            return;
-        }
-        StringBuilder builder = new StringBuilder();
-        String ColumnNamesList = "Number,Summary,Work Log Author, Time";
-        builder.append(ColumnNamesList +"\n");
-
-        for(TicketLoggedWork item : records){
-            item.calculate();
-            for (Map.Entry<String, String> entry : item.timeByAuthors.entrySet()) {
-                builder.append(item.number + ',');
-                builder.append(item.summary + ',');
-                builder.append(entry.getKey() + ',');
-                builder.append("\"=" + entry.getValue() + "/172800\"");
-                builder.append('\n');
+            try {
+                while (products.readRecord()) {
+                    List<String> workLogged = new ArrayList<String>();
+                    for (int i = 0; i < indexesForLoggedWork.size(); i++) {
+                        if (!products.get((Integer) indexesForLoggedWork.get(i)).equalsIgnoreCase(""))
+                            workLogged.add(products.get((Integer) indexesForLoggedWork.get(i)));
+                    }
+                    records.add(new TicketLoggedWork(products.get(indexForIssueKey), workLogged, products.get(indexForSummaryKey)));
+                }
+                products.close();
+            } catch (IOException e2) {
+                System.out.println("Error occured while working with file: " + args[0]);
+                e2.printStackTrace();
+                return;
             }
+
+            PrintWriter pw = null;
+            try {
+                pw = new PrintWriter(new File(pathToFileCreate));
+            } catch (FileNotFoundException e3) {
+                System.out.println("Could not create file: " + args[1]);
+                e3.printStackTrace();
+                return;
+            }
+            StringBuilder builder = new StringBuilder();
+            String ColumnNamesList = "Number,Summary,Work Log Author, Time";
+            builder.append(ColumnNamesList + "\n");
+
+            for (TicketLoggedWork item : records) {
+
+                if(reportMonth.isEmpty())
+                    item.calculate();
+                else
+                    item.calculate(reportMonth);
+
+                for (Map.Entry<String, String> entry : item.timeByAuthors.entrySet()) {
+                    builder.append(item.number + ',');
+                    builder.append(item.summary + ',');
+                    builder.append(entry.getKey() + ',');
+                    builder.append("\"=" + entry.getValue() + "/172800\"");
+                    builder.append('\n');
+                }
+            }
+            pw.write(builder.toString());
+            pw.close();
+            System.out.println("Transformation completed!");
         }
-        pw.write(builder.toString());
-        pw.close();
-        System.out.println("Transformation completed!");
+
+
     }
 
 }
--- src/main/java/TicketLoggedWork.java
+++ src/main/java/TicketLoggedWork.java
@@ -10,8 +10,18 @@ public class TicketLoggedWork {
     String number;
     String summary;
     List<String> loggedWork;
+    List<String> loggedWorkFinal;
+
+    class RecordInfo{
+        String time;
+        String day;
+        String month;
+        String year;
+    }
 
     Map<String, String> timeByAuthors = new HashMap<String, String>();
+    Map<String, RecordInfo> timesRecord = new HashMap<String, RecordInfo>();
+
 
     void calculate(){
         for(String comment : loggedWork){
@@ -23,12 +33,37 @@ public class TicketLoggedWork {
             name = parts[parts.length - 2];
             time = parts[parts.length - 1];
 
+
+
             if(timeByAuthors.containsKey(name)){
                 String a = timeByAuthors.get(name);
                 timeByAuthors.put(name, String.valueOf(Integer.parseInt(a) + Integer.parseInt(time)));
             }
             else{
                 timeByAuthors.put(name, String.valueOf(Integer.parseInt(time)));
+            }
+        }
+    }
+
+    void calculate(String month){
+
+        for(String comment : loggedWork){
+            String name;
+            String time;
+
+            String[] parts = comment.split(";");
+
+            name = parts[parts.length - 2];
+            time = parts[parts.length - 1];
+
+            if(month.equalsIgnoreCase(parts[parts.length - 3].split("/")[1])) {
+                if(timeByAuthors.containsKey(name)){
+                    String a = timeByAuthors.get(name);
+                    timeByAuthors.put(name, String.valueOf(Integer.parseInt(a) + Integer.parseInt(time)));
+                }
+                else{
+                    timeByAuthors.put(name, String.valueOf(Integer.parseInt(time)));
+                }
             }
         }
     }
